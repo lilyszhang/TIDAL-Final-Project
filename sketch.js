09a3ca6c0b2997934d03413214e2e234faec8308
sameersrivastava <sameer.srivastava95@gmail.com>
2017-05-17 11:44:52 -0500

    Added text above the wave.

--- sketch.js
+++ sketch.js
@@ -6,13 +6,24 @@ TopCodes.setVideoFrameCallback("video-canvas", function(jsonString) {
     ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     ctx.fillStyle = "black";
 
+    var light = {
+      16: 'radio',
+      8: 'microwave',
+      4: 'infrared',
+      2: 'visible light',
+      1: 'ultraviolet',
+      0.5: 'x-ray',
+      0.25: 'gamma ray'
+    };
+
     var i;
     var sineWave = [];
+    //Put topcodes in system
     for (i=0; i < topcodes.length; i++) {
         if(topcodes[i].code == 93) {
           sineWave.push({
             x:topcodes[i].x,
-            frequency: 5 //Higher frequency (default is 10)
+            frequencyMultiplier: 0.5 //Higher frequency (default is 10)
           }); 
           ctx.beginPath();
           ctx.moveTo(topcodes[i].x, 0);
@@ -22,7 +33,7 @@ TopCodes.setVideoFrameCallback("video-canvas", function(jsonString) {
         } else if (topcodes[i].code == 155) {
           sineWave.push({
             x:topcodes[i].x,
-            frequency: 20 //Lower frequency (default is 10)
+            frequencyMultiplier: 2 //Lower frequency (default is 10)
           });
           ctx.beginPath();
           ctx.moveTo(topcodes[i].x, 0);
@@ -32,48 +43,53 @@ TopCodes.setVideoFrameCallback("video-canvas", function(jsonString) {
         }
     }
     
-
-
     //Draw sineWave waves
     sineWave.sort(function(a,b){
       return a.x - b.x;
     });
 
-    console.log(sineWave);
-
+    var currentFrequency = 2;
     //Draw first part of the wave
     var firstSection = (sineWave[0]) ? sineWave[0].x : canvasWidth;
+    var yIntercept = 300;
 
-    var counter = 0, x=0,y=180;
+    var counter = 0, x=0,y = yIntercept;
     var increase = 90/180*Math.PI / 9;
     ctx.beginPath();
-    for(i = 0; i <= firstSection; i += 10){
+    for(i = 0; i <= firstSection; i += currentFrequency){
         ctx.moveTo(x,y);
         x = i;
-        y =  180 - Math.sin(counter) * 120;
+        y = yIntercept - Math.sin(counter) * 60;
         counter += increase; 
         ctx.lineTo(x,y);
         ctx.stroke();
     }
     ctx.closePath();
 
+    //Write the text
+    ctx.font="20px Georgia";
+    ctx.textAlign="center";
+    ctx.fillText(light[currentFrequency],firstSection/2,100);
+
 
     //If more sections exist
     for(var sectionIndex = 0; sectionIndex < sineWave.length; sectionIndex += 1){
       nextSectionX = ((sectionIndex + 1) < sineWave.length) ? 
         sineWave[sectionIndex + 1].x : canvasWidth;
-      var counter = 0, x=sineWave[sectionIndex].x,y=180;
-      var increase = 90/180*Math.PI / 9;
+      var counter = 0, x = sineWave[sectionIndex].x,y = yIntercept;
+      currentFrequency *= sineWave[sectionIndex].frequencyMultiplier;
+      console.log(currentFrequency);
       ctx.beginPath();
-      for(i = sineWave[sectionIndex].x; i <= nextSectionX; i += sineWave[sectionIndex].frequency){
+      for(i = sineWave[sectionIndex].x; i <= nextSectionX; i += currentFrequency){
           ctx.moveTo(x,y);
           x = i;
-          y =  180 - Math.sin(counter) * 120;
+          y = yIntercept - Math.sin(counter) * 60;
           counter += increase; 
           ctx.lineTo(x,y);
           ctx.stroke();
       }
       ctx.closePath();
+      ctx.fillText(light[currentFrequency], (sineWave[sectionIndex].x + nextSectionX)/2, 100);
     }
 });
 
